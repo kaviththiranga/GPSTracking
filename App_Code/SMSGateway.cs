1b34f2f61638b80b9846378f35903eb95333886d
klokuhewage <rc404mekt@gmail.com>
2013-07-28 12:18:50 +0530

    Added Local Database
    Signed-off-by: klokuhewage <rc404mekt@gmail.com>

---
+++ App_Code/SMSGateway.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading;
+using org.smslib;
+using org.smslib.modem;
+using Comm2IP;
+/// <summary>
+/// Summary description for SMSGateway
+/// </summary>
+public class SMSGateway
+{
+	public SMSGateway()
+	{
+
+	}
+
+    public static List<InboundMessage> getNewMessages() {
+
+        return new List<InboundMessage>();
+    }
+
+    public static void sendMessage(string number, string msg) { 
+    
+    }
+}
---
+++ App_Code/VehicleManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for VehicleManager
+/// </summary>
+public class VehicleManager
+{
+	public VehicleManager()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+}
